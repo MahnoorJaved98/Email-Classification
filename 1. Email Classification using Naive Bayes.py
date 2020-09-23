a437512f4becb4d1cfb842337a6cae92a19ec351
Mahnoor Javed <engr.mahnoorjaved@gmail.com>
2020-09-23 19:46:51 +0500

    Update and rename Email Classification using Naive Bayes.py to 1. Email Classification using Naive Bayes.py

--- 1. Email Classification using Naive Bayes.py
+++ 1. Email Classification using Naive Bayes.py
@@ -4,22 +4,23 @@
     Sara has label 0
     Chris has label 1
 """   
+
 import sys
 from time import time
 sys.path.append("C:\\Users\\HP\\Desktop\\ML Code\\")
 from email_preprocess import preprocess
 import numpy as np
 
-# using the Gaussian Bayes algorithm for classification of emails.
-# the algorithm is imported from the sklearn library
+#using the Gaussian Bayes algorithm for classification of emails.
+#the algorithm is imported from the sklearn library
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score
 
-# initializaing the test and train features and labels
-# the function preprocess is imported from email_preprocess.py 
+#initializaing the test and train features and labels
+#the function preprocess is imported from email_preprocess.py 
 features_train, features_test, labels_train, labels_test = preprocess()
 
-# defining the classifier
+#defining the classifier
 clf = GaussianNB()
 
 #predicting the time of train and testing
@@ -30,6 +31,7 @@ t1 = time()
 pred = clf.predict(features_test)
 print("Predicting time:", round(time()-t1, 3), "s\n")
 
+#calculating and printing the accuracy
 print("Accuracy of Naive Bayes: ", accuracy_score(pred,labels_test))
 
 
